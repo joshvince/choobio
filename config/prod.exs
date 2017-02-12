35d3ee008883f56341314137e3b17f10114bca7d
Josh <joshmvince@gmail.com>
2017-02-12 16:57:09 +0000

    different port

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,4 @@ use Mix.Config
 config :commuter, :tfl_api, Commuter.Tfl
 config :commuter,
   server: true,
-  http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "choob-service.herokuapp.com", port: 443]
+  http: [port: 4000]
