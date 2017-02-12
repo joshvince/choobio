8b11e0703ff08bb1162d5fa87df85fc264d7ed08
Josh <joshmvince@gmail.com>
2017-02-12 16:55:29 +0000

    commas...

--- config/prod.exs
+++ config/prod.exs
@@ -3,4 +3,5 @@ use Mix.Config
 config :commuter, :tfl_api, Commuter.Tfl
 config :commuter,
   server: true,
-  http: [port: {:system, "PORT"}]
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "choob-service.herokuapp.com", port: 443]
