4a985007cec9cca00a6678baf376f386d47e1f00
Josh <joshmvince@gmail.com>
2017-03-10 17:28:06 +0000

    spawn platform genservers on app startup

--- lib/choobio/stations/station.ex
+++ lib/choobio/stations/station.ex
@@ -1,9 +1,11 @@
 defmodule Choobio.Station do
   @moduledoc """
-  Handles the creation of station models for insertion into the DB.
+  Handles the creation of, and interaction with, station models in the DB.
   """
   use Ecto.Schema
+  import Ecto.Query, only: [from: 2]
   alias __MODULE__, as: Station
+  alias Choobio.Repo
 
   schema "stations" do
     field :name, :string
@@ -13,20 +15,36 @@ defmodule Choobio.Station do
     timestamps()
   end
 
+  # Application Setup
+
+  # Stations are only created when the seeds are ran.
+  def create(params) do
+    changeset(%Station{}, params)
+    |> Repo.insert!
+  end
+
   def changeset(struct, params \\ %{}) do
     struct
     |> Ecto.Changeset.cast(params, [:name, :naptanId])
     |> Ecto.Changeset.put_assoc(:lines, get_lines(params.lines))
   end
 
-  def get_lines(line_ids) do
-    Enum.map(line_ids, &Choobio.Repo.get_by(Choobio.Line, line_id: &1))
+  defp get_lines(line_ids) do
+    Enum.map(line_ids, &Repo.get_by(Choobio.Line, line_id: &1))
   end
 
-  # Stations are only created when the seeds are ran.
-  def create(params) do
-    changeset(%Station{}, params)
-    |> Choobio.Repo.insert!
+  # API for Platforms
+
+  def get_all_stations do
+    Repo.all(Station)
+    |> Repo.preload(:lines)
   end
+
+  def get_one_station(natpan_id) do
+    Repo.get_by(Station, naptanId: natpan_id)
+    |> Repo.preload(:lines)
+  end
+
+
 
 end
